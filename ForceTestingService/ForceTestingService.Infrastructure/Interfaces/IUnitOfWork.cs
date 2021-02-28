71e8d4ff54c4d648a8b38e7d874427330064f687
F0rsV <vforsyuk001@gmail.com>
2021-02-28 19:50:42 +0200

    Repository and UnitOfWork added

---
+++ ForceTestingService/ForceTestingService.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using ForceTestingService.Infrastructure.Entities;
+
+namespace ForceTestingService.Infrastructure.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Answer> AnswerRepository { get; }
+        IRepository<Question> QuestionRepository { get; }
+        IRepository<Subject> SubjectRepository { get; }
+        IRepository<Test> TestRepository { get; }
+        IRepository<Topic> TopicRepository { get; }
+        IRepository<User> UserRepository { get; }
+
+        Task CommitAsync();
+        Task RollbackAsync();
+
+    }
+}
---
+++ ForceTestingService/ForceTestingService.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using ForceTestingService.Infrastructure.Context;
+using ForceTestingService.Infrastructure.Entities;
+using ForceTestingService.Infrastructure.Interfaces;
+using ForceTestingService.Infrastructure.Repositories;
+
+namespace ForceTestingService.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        private IRepository<Answer> _answerRepository;
+        public IRepository<Answer> AnswerRepository => _answerRepository ??= new GenericRepository<Answer>(_context);
+
+        private IRepository<Question> _questionRepository;
+        public IRepository<Question> QuestionRepository => _questionRepository ??= new GenericRepository<Question>(_context);
+
+        private IRepository<Subject> _subjectRepository;
+        public IRepository<Subject> SubjectRepository => _subjectRepository ??= new GenericRepository<Subject>(_context);
+
+        private IRepository<Test> _testRepository;
+        public IRepository<Test> TestRepository => _testRepository ??= new GenericRepository<Test>(_context);
+
+        private IRepository<Topic> _topicRepository;
+        public IRepository<Topic> TopicRepository => _topicRepository ??= new GenericRepository<Topic>(_context);
+
+        private IRepository<User> _userRepository;
+        public IRepository<User> UserRepository => _userRepository ??= new GenericRepository<User>(_context);
+
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        private bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
+        }
+
+        public void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+    }
+}
