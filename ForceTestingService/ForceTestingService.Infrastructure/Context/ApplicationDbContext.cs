4de22214b4f616634cc5adb5cf5fe0a3aa10d064
F0rsV <vforsyuk001@gmail.com>
2021-02-28 19:50:24 +0200

    DbContext added

---
+++ ForceTestingService/ForceTestingService.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ForceTestingService.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForceTestingService.Infrastructure.Context
+{
+    public class ApplicationDbContext: IdentityDbContext<User>
+    {
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
