d6e62ba104b7df22527b96d4fab82dc75e3be7df
F0rsV <vforsyuk001@gmail.com>
2021-03-08 17:48:42 +0200

    fix: Test entity added property

--- ForceTestingService/ForceTestingService.Infrastructure/Entities/Test.cs
+++ ForceTestingService/ForceTestingService.Infrastructure/Entities/Test.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public int NumOfQuestions { get; set; }
         public int AmountOfSeconds { get; set; }
+        public int NumOfTries { get; set; }
 
         public Topic Topic { get; set; }
         public int TopicId { get; set; }
