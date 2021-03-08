3aa6c8d098c391ad33441ea405043899e92fd4c5
F0rsV <vforsyuk001@gmail.com>
2021-03-08 21:18:33 +0200

    Add generic mapper and mapper interface

---
+++ ForceTestingService/ForceTestingService.ApplicationCore/Interfaces/IMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForceTestingService.ApplicationCore.Interfaces
+{
+    public interface IMapper <TEntity, TDto>
+    {
+        TEntity Map(TDto dto);
+        TDto Map(TEntity entity);
+
+        IEnumerable<TEntity> Map(IEnumerable<TDto> dataTransferObjects, Action<TEntity> callback = null);
+        IEnumerable<TDto> Map(IEnumerable<TEntity> entities, Action<TDto> callback = null);
+    }
+}
---
+++ ForceTestingService/ForceTestingService.ApplicationCore/Mappers/GenericMapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ForceTestingService.ApplicationCore.Interfaces;
+
+namespace ForceTestingService.ApplicationCore.Mappers
+{
+    public abstract class GenericMapper<TEntity, TDto> : IMapper<TEntity, TDto>
+    {
+        public abstract TEntity Map(TDto dto);
+        public abstract TDto Map(TEntity entity);
+
+        public IEnumerable<TEntity> Map(IEnumerable<TDto> dataTransferObjects, Action<TEntity> callback = null)
+        {
+            var mappingResult = new List<TEntity>();
+            foreach (var dto in dataTransferObjects)
+            {
+                var mappedEntity = Map(dto);
+                if (mappedEntity != null)
+                {
+                    callback?.Invoke(mappedEntity);
+                    mappingResult.Add(mappedEntity);
+                }
+            }
+
+            return mappingResult;
+        }
+
+        public IEnumerable<TDto> Map(IEnumerable<TEntity> entities, Action<TDto> callback = null)
+        {
+            var mappingResult = new List<TDto>();
+            foreach (var entity in entities)
+            {
+                var mappedDto = Map(entity);
+                if (mappedDto != null)
+                {
+                    callback?.Invoke(mappedDto);
+                    mappingResult.Add(mappedDto);
+                }
+            }
+
+            return mappingResult;
+        }
+    }
+}
