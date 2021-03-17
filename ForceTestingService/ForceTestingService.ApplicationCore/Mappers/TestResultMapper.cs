06488788c905c485ce884a68cfb6cefd759f00c1
F0rsV <vforsyuk001@gmail.com>
2021-03-17 17:01:52 +0200

    Added TestResultMapper

---
+++ ForceTestingService/ForceTestingService.ApplicationCore/Mappers/TestResultMapper.cs
@@ -0,0 +1,30 @@
+﻿using ForceTestingService.ApplicationCore.DTO;
+using ForceTestingService.Infrastructure.Entities;
+
+namespace ForceTestingService.ApplicationCore.Mappers
+{
+    public class TestResultMapper: GenericMapper<TestResult, TestResultDto>
+    {
+        public override TestResult Map(TestResultDto dto)
+        {
+            return new TestResult()
+            {
+                Id = dto.Id,
+                Score = dto.Score,
+                Date = dto.Date,
+                StudentUserId = dto.StudentUserId
+            };
+        }
+
+        public override TestResultDto Map(TestResult entity)
+        {
+            return new TestResultDto()
+            {
+                Id = entity.Id,
+                Score = entity.Score,
+                Date = entity.Date,
+                StudentUserId = entity.StudentUserId
+            };
+        }
+    }
+}
