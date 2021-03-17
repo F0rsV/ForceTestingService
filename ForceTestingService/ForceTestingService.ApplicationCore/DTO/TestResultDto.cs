79ae9d9d710555486af7d3e051a037f9fac15700
F0rsV <vforsyuk001@gmail.com>
2021-03-17 17:01:39 +0200

    Added TestResultDto

---
+++ ForceTestingService/ForceTestingService.ApplicationCore/DTO/TestResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ForceTestingService.ApplicationCore.DTO
+{
+    public class TestResultDto
+    {
+        public int Id { get; set; }
+        public float Score { get; set; }
+        public DateTime Date { get; set; }
+
+        public int StudentUserId { get; set; }
+    }
+}
