ab11cf6f82a80efee846247712ed87d0e89a9c46
F0rsV <vforsyuk001@gmail.com>
2021-03-17 17:00:58 +0200

    Added TestResult entity

---
+++ ForceTestingService/ForceTestingService.Infrastructure/Entities/TestResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ForceTestingService.Infrastructure.Entities
+{
+    public class TestResult
+    {
+        public int Id { get; set; }
+        public float Score { get; set; }
+        public DateTime Date { get; set; }
+
+        public User StudentUser { get; set; }
+        public int StudentUserId { get; set; }
+    }
+}
