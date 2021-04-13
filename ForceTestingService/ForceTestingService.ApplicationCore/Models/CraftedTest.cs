eeec24f9a502b2ba35973cff26a7ed0b4276f013
F0rsV <vforsyuk001@gmail.com>
2021-04-13 23:23:00 +0300

    Add BLL models

---
+++ ForceTestingService/ForceTestingService.ApplicationCore/Models/CraftedTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ForceTestingService.ApplicationCore.DTO;
+using ForceTestingService.Infrastructure.Entities;
+
+namespace ForceTestingService.ApplicationCore.Models
+{
+    public class CraftedTest
+    {
+        public User User { get; set; }
+        public TestBaseDto TestBaseDto { get; set; }
+        public DateTime Date { get; set; }
+        public float Score { get; set; }
+
+        public IEnumerable<QuestionDto> Questions { get; set; }
+
+        public CraftedTest(){}
+
+        public CraftedTest(User user, TestBaseDto testBaseDto)
+        {
+            User = user;
+            TestBaseDto = testBaseDto;
+            Date = DateTime.UtcNow;
+            Score = 0;
+            Questions = new List<QuestionDto>();
+        }
+    }
+}
---
+++ ForceTestingService/ForceTestingService.ApplicationCore/Models/StudentTest.cs
@@ -0,0 +1,29 @@
+﻿using ForceTestingService.Infrastructure.Entities;
+
+namespace ForceTestingService.ApplicationCore.Models
+{
+    public class StudentTest
+    {
+        public int TestBaseId { get; set; }
+
+        public string TestName { get; set; }
+        public string SubjectName { get; set; }
+        public string TopicName { get; set; }
+        public int NumOfQuestions { get; set; }
+        public int AmountOfSeconds { get; set; }
+        public int NumOfTriesLeft { get; set; }
+        public bool IsPassed { get; set; }
+
+        public StudentTest(TestBase testBase, int numOfTriesLeft, bool isPassed)
+        {
+            TestBaseId = testBase.Id;
+            TestName = testBase.Name;
+            SubjectName = testBase.Topic.Subject.Name;
+            TopicName = testBase.Topic.Name;
+            NumOfQuestions = testBase.NumOfQuestions;
+            AmountOfSeconds = testBase.AmountOfSeconds;
+            NumOfTriesLeft = numOfTriesLeft;
+            IsPassed = isPassed;
+        }
+    }
+}
