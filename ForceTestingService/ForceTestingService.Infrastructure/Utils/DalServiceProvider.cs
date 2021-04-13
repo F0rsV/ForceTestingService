9e0d0d303dee5dcb8425ec6458218a1a2a9cdbde
F0rsV <vforsyuk001@gmail.com>
2021-04-13 23:24:01 +0300

    Add DAL Utils

---
+++ ForceTestingService/ForceTestingService.Infrastructure/Utils/DalServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using ForceTestingService.Infrastructure.Context;
+using ForceTestingService.Infrastructure.Entities;
+using ForceTestingService.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ForceTestingService.Infrastructure.Utils
+{
+    public static class DalServiceProvider
+    {
+        public static IServiceCollection RegisterDalServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddIdentity<User, IdentityRole<int>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+
+            return services;
+        }
+    }
+}
