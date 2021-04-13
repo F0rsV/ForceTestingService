e1d1f3d2433fd18f4c4d97f10a6e6bff8a626ac8
F0rsV <vforsyuk001@gmail.com>
2021-04-13 23:25:09 +0300

    Add RoleInitializer

---
+++ ForceTestingService/ForceTestingService.WEB/Utils/RoleInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using ForceTestingService.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace ForceTestingService.WEB.Utils
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            if (await roleManager.FindByNameAsync("teacher") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("teacher"));
+            }
+            if (await roleManager.FindByNameAsync("student") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("student"));
+            }
+        }
+    }
+}
