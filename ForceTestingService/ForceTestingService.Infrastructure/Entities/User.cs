34e75c0374880d50a32e0bf893a8d057458db4e5
F0rsV <vforsyuk001@gmail.com>
2021-04-13 23:17:56 +0300

    Update user entity

--- ForceTestingService/ForceTestingService.Infrastructure/Entities/User.cs
+++ ForceTestingService/ForceTestingService.Infrastructure/Entities/User.cs
@@ -2,7 +2,7 @@
 
 namespace ForceTestingService.Infrastructure.Entities
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<int>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
