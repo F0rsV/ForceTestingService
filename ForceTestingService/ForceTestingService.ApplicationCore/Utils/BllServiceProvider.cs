43e6bc47022f1ce7f9d582dd78d4bb82f3f25751
F0rsV <vforsyuk001@gmail.com>
2021-04-13 23:23:22 +0300

    Add BLL Utils

---
+++ ForceTestingService/ForceTestingService.ApplicationCore/Utils/BllServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using ForceTestingService.ApplicationCore.Interfaces;
+using ForceTestingService.ApplicationCore.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ForceTestingService.ApplicationCore.Utils
+{
+    public static class BllServiceProvider
+    {
+        public static IServiceCollection RegisterBllServices(this IServiceCollection services)
+        {
+            services.AddTransient<ISubjectService, SubjectService>();
+            services.AddTransient<ITopicService, TopicService>();
+            services.AddTransient<IQuestionService, QuestionService>();
+            services.AddTransient<IAnswerService, AnswerService>();
+            services.AddTransient<ITestBaseService, TestBaseService>();
+            services.AddTransient<IStudentTestService, StudentTestService>();
+            services.AddTransient<ITestResultService, TestResultService>();
+            
+            return services;
+        }
+    }
+}
