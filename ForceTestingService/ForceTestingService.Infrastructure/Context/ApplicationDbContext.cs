d16368167610e7b67db4f6fee1793994d73a8058
F0rsV <vforsyuk001@gmail.com>
2021-04-13 23:18:55 +0300

    Update DbContext

--- ForceTestingService/ForceTestingService.Infrastructure/Context/ApplicationDbContext.cs
+++ ForceTestingService/ForceTestingService.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using ForceTestingService.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ForceTestingService.Infrastructure.Context
 {
-    public class ApplicationDbContext: IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Question> Questions { get; set; }
