f7e9cbf7c01500d0783c0682290e761e91c51151
F0rsV <vforsyuk001@gmail.com>
2021-05-23 13:31:03 +0300

    add docker-compose

--- ForceTestingService/ForceTestingService.WEB/Startup.cs
+++ ForceTestingService/ForceTestingService.WEB/Startup.cs
@@ -34,7 +34,7 @@ namespace ForceTestingService.WEB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.RegisterDalServices(Configuration.GetConnectionString("DefaultConnection"));
+            services.RegisterDalServices(Configuration.GetConnectionString("DescConnection")); // DefaultConnection
             services.RegisterBllServices();
             services.AddControllersWithViews();
         }
@@ -52,7 +52,7 @@ namespace ForceTestingService.WEB
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
